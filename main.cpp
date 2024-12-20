2dcc87808a442633064e2b0f12c969bd71e9ce2b
Namtheskidder <nguyenhoa1952nb@gmail.com>
2024-12-20 21:30:36 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int c;
+int main()
+{
+    cout<<"[+]dienthaokiller-made in phap su trieu tien"<<endl<<endl<<"nhap lua chon cua ban (1=ok,2=out):";
+    cin>>c;
+    if(c==1){
+        for(int i=0;i<=1000;i++){
+            cout<<"[+] dg phong ten lua vao nha thg dienthao "<<i<<"/1000"<<endl;
+        }
+    }
+
+    cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+    cout<<"[+] dienthao da die";
+    return 0;
+}
